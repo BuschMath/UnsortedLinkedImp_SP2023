d8b9ff70af655498b72434730753eced14072d74
BuschMath <busch@iowacentral.edu>
2023-02-01 11:14:00 -0600

    Add project files.

---
+++ UnsortedLinkedImp_SP2023/UnsortedListL.cpp
@@ -0,0 +1,69 @@
+#include "UnsortedListL.h"
+
+UnsortedListL::UnsortedListL()
+{
+	head = nullptr;
+	loc = nullptr;
+	listSize = 0;
+}
+
+UnsortedListL::~UnsortedListL()
+{
+}
+
+void UnsortedListL::MakeEmpty()
+{
+}
+
+bool UnsortedListL::IsFull()
+{
+	return false;
+}
+
+int UnsortedListL::GetLength()
+{
+	return 0;
+}
+
+ItemType UnsortedListL::GetItem(ItemType item, bool& found)
+{
+	return ItemType();
+}
+
+bool UnsortedListL::PutItem(ItemType item)
+{
+	if (!IsFull())
+	{
+		Node* temp = new Node;
+		temp->item = item;
+
+		if (head == nullptr)
+		{
+			temp->next = nullptr;
+			head = temp;
+		}
+		else
+		{
+			temp->next = head;
+			head = temp;
+		}
+
+		return true;
+	}
+
+	return false;
+}
+
+bool UnsortedListL::DeleteItem(ItemType item)
+{
+	return false;
+}
+
+void UnsortedListL::ResetList()
+{
+}
+
+ItemType UnsortedListL::GetNextItem()
+{
+	return ItemType();
+}
---
+++ UnsortedLinkedImp_SP2023/UnsortedListL.h
@@ -0,0 +1,33 @@
+#ifndef UNSORTEDLISTL_H
+#define UNSORTEDLISTL_H
+
+#include "ItemType.h"
+
+struct Node
+{
+	ItemType item;
+	Node* next;
+};
+
+class UnsortedListL
+{
+public:
+	UnsortedListL();
+	~UnsortedListL();
+
+	void MakeEmpty();
+	bool IsFull();
+	int GetLength();
+	ItemType GetItem(ItemType item, bool& found);
+	bool PutItem(ItemType item);
+	bool DeleteItem(ItemType item);
+	void ResetList();
+	ItemType GetNextItem();
+
+private:
+	Node* head;
+	Node* loc;
+	int listSize;
+};
+
+#endif // !UNSORTEDLISTL_H
